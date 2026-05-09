f965319953bf0fadbeb436bfe3a0dadb7a8e008d
Ko Fujimura <fujimura.ko@gmail.com>
2026-05-09 23:27:49 +0000

    fix set turbopack root

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['35.202.52.156'],
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
